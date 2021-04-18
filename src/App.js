53995c9210b0711d0c56ab01d3f1905fa6a5a005
Abhishek Kumar <abhishek020898@gmail.com>
2021-04-18 12:24:02 +0530

    App.js updated

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import Book from './components/Book';
 import "./App.css";
 const App = () => {
 
-    const [books, setBooks] = useState([])
+    const [books, setBooks] = useState([]);
+    const [search, setSearch] = useState('');
+    const [query, setQuery] = useState('');
 
     useEffect(()=>{
         getBooks();
-    },[]);
+    },[query]);
 
     const getBooks = async () =>{
         const response = await fetch(`https://www.googleapis.com/books/v1/volumes?q=search+terms`);
@@ -16,16 +18,28 @@ const App = () => {
         console.log(data);
     }
 
+    const handleSubmit = (e) =>{
+        e.preventDefault();
+        setQuery(search);
+    }
+
 
     return (
         <div className="App">
-            <form className="search-form">
-                <input className="search-bar" type="text" placeholder="Search Book"/>
+            <form className="search-form" onSubmit={handleSubmit}>
+                <input className="search-bar" type="text" value="search" placeholder="Search Book" onChange={e => setSearch(e.target.value)}/>
                 <button className="search-button" type="submit">Search</button>
             </form>
             <div>
                 <div className="abcd">
-                    {books.map(book => (
+                    {books.filter((val)=>{
+                        if(search === "")
+                        {
+                            return val;
+                        }else if(val.volumeInfo.title.toLowerCase().includes(search.toLocaleLowerCase())){
+                            return val;
+                        }
+                    }).map(book => (
                         <Book
                         image={book.volumeInfo.imageLinks.thumbnail}
                         author={book.volumeInfo.authors}
